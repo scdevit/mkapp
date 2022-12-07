590ddee77574bd9c6e824a68e504b6885937708f
okki23 <okkisetyawan@gmail.com>
2022-12-08 04:25:37 +0700

    updateah

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,23 +1,23 @@
-// module.exports = {
-//   apps: [{
-//     name : "mkapp",
-//     script: "npm run build && npm run start"
-//   }]
-// }
-
 module.exports = {
-  apps : [{
-    name: 'mkapp',
-    script: 'node_modules/.bin/next',
-    args:'start',
-    cwd:"/var/www/html/mkapp",
-    instances: 3,
-    autorestart: true,
-    watch: false,
-    max_memory_restart: '1G',
-    env: {
-        NODE_ENV: "production",
-        PORT: 4000
-    },
+  apps: [{
+    name : "mkapp",
+    script: "npm run build && npm run start"
   }]
-};
+}
+
+// module.exports = {
+//   apps : [{
+//     name: 'mkapp',
+//     script: 'node_modules/.bin/next',
+//     args:'start',
+//     cwd:"/var/www/html/mkapp",
+//     instances: 3,
+//     autorestart: true,
+//     watch: false,
+//     max_memory_restart: '1G',
+//     env: {
+//         NODE_ENV: "production",
+//         PORT: 4000
+//     },
+//   }]
+// };
