18e468c957ba2498fbefa531583d9c6f45d56856
okki23 <okkisetyawan@gmail.com>
2022-12-09 09:01:23 +0700

    cobalagi

--- pages/index.tsx
+++ pages/index.tsx
@@ -23,7 +23,7 @@ export default function Home() {
 
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
+            <h2>Mau bisa? baca dokumentasi yang bener jangan maen hape mulu!  &rarr;</h2>
             <p>Find in-depth information about Next.js features and API.</p>
           </a>
 
